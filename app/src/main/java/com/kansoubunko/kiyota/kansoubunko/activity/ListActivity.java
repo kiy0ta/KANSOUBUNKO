7fdfc8cc838a304026658a31de9ed7be21cc51b5
m.kiyota <m.kiyota@secure-i.jp>
2019-09-06 13:56:50 +0900

    ListActivity ui fix

--- app/src/main/java/com/kansoubunko/kiyota/kansoubunko/activity/ListActivity.java
+++ app/src/main/java/com/kansoubunko/kiyota/kansoubunko/activity/ListActivity.java
@@ -4,18 +4,43 @@ package com.kansoubunko.kiyota.kansoubunko.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
 
 import com.kansoubunko.kiyota.kansoubunko.R;
+import com.kansoubunko.kiyota.kansoubunko.adapter.BookListGridAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
     public static Intent getStartIntent(MainActivity mainActivity) {
         return new Intent(mainActivity, ListActivity.class);
     }
 
+    // 表示する画像の名前（拡張子無し）
+    private String members[] = {
+            "no_book_img", "no_book_img", "no_book_img", "no_book_img", "no_book_img",
+            "no_book_img", "no_book_img", "no_book_img", "no_book_img", "no_book_img"
+    };
+
+    // Resource IDを格納するarray
+    private List<Integer> imgList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+// for-each member名をR.drawable.名前としてintに変換してarrayに登録
+        for (String member : members) {
+            int imageId = getResources().getIdentifier(
+                    member, "drawable", getPackageName());
+            imgList.add(imageId);
+        }
+
+        GridView bookListGridView = findViewById(R.id.list_book);
+        BookListGridAdapter adapter = new BookListGridAdapter(this, R.layout.item_book_list, imgList, members);
+        bookListGridView.setAdapter(adapter);
 
     }
 }
---
+++ app/src/main/java/com/kansoubunko/kiyota/kansoubunko/adapter/BookListGridAdapter.java
@@ -0,0 +1,88 @@
+package com.kansoubunko.kiyota.kansoubunko.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kansoubunko.kiyota.kansoubunko.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookListGridAdapter extends BaseAdapter {
+
+    private static Context sContext;
+    private static int sItemResourceId;
+    public static List<Integer> sImageList = new ArrayList<>();
+    private static String[] sImageMembers = null;
+    private LayoutInflater inflater;
+
+
+    //新規登録したあとにMainでよびだしている初期化処理
+//    public void initList(List<ProteinEntity> mEntityList) {
+//        this.mEntityList = mEntityList;
+//        hashmap = new HashMap<Date, String>();
+//    }
+
+    //コンストラクタ
+    public BookListGridAdapter(Context context, int itemLayoutId, List<Integer> integerList, String[] members) {
+        sContext = context;
+        this.inflater = (LayoutInflater)
+                sContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        sItemResourceId = itemLayoutId;
+        sImageList = integerList;
+        sImageMembers = members;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+        if (convertView == null) {
+            // main.xml の <GridView .../> に grid_items.xml を inflate して convertView とする
+            convertView = inflater.inflate(sItemResourceId, parent, false);
+            // ViewHolder を生成
+            holder = new ViewHolder();
+
+            holder.bookImage = convertView.findViewById(R.id.item_book_image);
+            holder.bookTitleText = convertView.findViewById(R.id.item_book_text);
+
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.bookImage.setImageResource(sImageList.get(position));
+        holder.bookTitleText.setText(sImageMembers[position]);
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        // List<String> imgList の全要素数を返す
+        return sImageList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    //Viewに渡すほんのタイトルと画像
+    public class ViewHolder {
+        public TextView bookTitleText;
+        public ImageView bookImage;
+    }
+
+}
