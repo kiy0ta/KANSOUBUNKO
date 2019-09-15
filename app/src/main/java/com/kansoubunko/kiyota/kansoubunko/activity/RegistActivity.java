1a3d85580ff34efd161d1996306ff66ac693ebd8
m.kiyota <m.kiyota@secure-i.jp>
2019-09-15 21:03:07 +0900

    # カメラとギャラリーのパーミッションを追加

--- app/src/main/java/com/kansoubunko/kiyota/kansoubunko/activity/RegistActivity.java
+++ app/src/main/java/com/kansoubunko/kiyota/kansoubunko/activity/RegistActivity.java
@@ -1,14 +1,20 @@
 package com.kansoubunko.kiyota.kansoubunko.activity;
 
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kansoubunko.kiyota.kansoubunko.R;
@@ -38,6 +44,9 @@ public class RegistActivity extends AppCompatActivity {
     private BookReviewGridAdapter adapter;
     private GridView bookReviewGridView;
     private TextView bookTitleTextView;
+    private ImageView bookImageView;
+    private Uri m_uri;
+    private static final int REQUEST_CHOOSER = 1000;
 
     public static Intent getStartIntent(MainActivity mainActivity) {
         return new Intent(mainActivity, RegistActivity.class);
@@ -78,6 +87,16 @@ public class RegistActivity extends AppCompatActivity {
         bookReviewGridView = findViewById(R.id.regist_book_review);
         adapter = new BookReviewGridAdapter(this, R.layout.item_book_review, word);
         bookReviewGridView.setAdapter(adapter);
+
+        //画像表示用ビュー
+        bookImageView = findViewById(R.id.regist_book_img);
+        bookImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showGallery();
+            }
+        });
+
 
         //タイトル入力用ビュー
         bookTitleTextView = findViewById(R.id.regist_book_title);
@@ -155,6 +174,68 @@ public class RegistActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void showGallery() {
+
+        //カメラの起動Intentの用意
+        String photoName = System.currentTimeMillis() + ".jpg";
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, photoName);
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        m_uri = getContentResolver()
+                .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+        Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, m_uri);
+
+        // ギャラリー用のIntent作成
+        Intent intentGallery;
+        if (Build.VERSION.SDK_INT < 19) {
+            intentGallery = new Intent(Intent.ACTION_GET_CONTENT);
+            intentGallery.setType("image/*");
+        } else {
+            intentGallery = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intentGallery.addCategory(Intent.CATEGORY_OPENABLE);
+            intentGallery.setType("image/jpeg");
+        }
+        Intent intent = Intent.createChooser(intentCamera, "画像の選択");
+        intent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{intentGallery});
+        startActivityForResult(intent, REQUEST_CHOOSER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CHOOSER) {
+
+            if (resultCode != RESULT_OK) {
+                // キャンセル時
+                return;
+            }
+
+            Uri resultUri = (data != null ? data.getData() : m_uri);
+
+            if (resultUri == null) {
+                // 取得失敗
+                return;
+            }
+
+            // ギャラリーへスキャンを促す
+            MediaScannerConnection.scanFile(
+                    this,
+                    new String[]{resultUri.getPath()},
+                    new String[]{"image/jpeg"},
+                    null
+            );
+
+            // 画像を設定
+            bookImageView.setImageURI(resultUri);
+            adapter = new BookReviewGridAdapter(this, R.layout.item_book_review, word);
+            bookReviewGridView.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
     }
 
 }
